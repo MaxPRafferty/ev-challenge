b096b5e9e062c923b0172123c81af67cd9834696
Max Rafferty <maxprafferty@gmail.com>
2022-07-09 17:43:32 -0400

    spartan wireup

--- src/App.js
+++ src/App.js
@@ -5,17 +5,10 @@ function App() {
     return (
         <div className="App">
             <header className="App-header">
-                <h1>
-                    <a
-                        className="App-link"
-                        href="https://reactjs.org"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        Learn React
-                    </a>
-                </h1>
+                <h1>Electric Vehicle Performance Comparison</h1>
             </header>
+            <div>Data</div>
+            <footer>Max Rafferty, 2022</footer>
         </div>
     );
 }
