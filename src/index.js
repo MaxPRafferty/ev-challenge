99fff12b2669745d2a8718ef1f56aae668fac0aa
Max Rafferty <maxprafferty@gmail.com>
2022-07-09 18:18:04 -0400

    initial fetching

--- src/index.js
+++ src/index.js
@@ -1,14 +1,42 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+/* MPR, 2022/7/9: these imports provide a path, not data */
+import tripsPath from "./data/trips.csv";
+import inputsPath from "./data/inputs.csv";
+
+/* MPR, 2022/7/9: A formal store here would be overkill. Because our application
+ * structure is quite flat, keeping a simple top level data object and "threading
+ * the needle" to where it is needed will provide all the benefits without any
+ * overcomplex setup.
+ */
+let trips = [];
+let inputs = [];
+let data = {};
+
+window.EV_CONSTANTS = window.EV_CONSTANTS || {};
+const IS_PRODUCTION = !!window.EV_CONSTANTS.isProd;
+
+/* MPR, 2022/7/9: intentionally not using await to not block render during hydration */
+fetch(`${IS_PRODUCTION ? "/ev-challenge" : ""}${tripsPath}`)
+    .then((res) => res.text())
+    .then((data) => console.log(data))
+    .catch((err) => console.log(err));
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <App
+            production={IS_PRODUCTION}
+            data={data}
+            dataLength={Object.keys(data)}
+            trips={trips}
+            inputs={inputs}
+        />
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
